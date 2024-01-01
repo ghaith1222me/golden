70a744d55c2e47b24e4d2b2cc03715758856a104
ghaith1222me <155396743+ghaith1222me@users.noreply.github.com>
2024-01-01 23:17:02 +0100

    files

---
+++ vendor/sociallydev/config.php
@@ -0,0 +1,12 @@
+<?php  
+    define("DB_NAME", 'if0_35696207_tesst');
+    define("DB_USER", 'if0_35696207');
+    define("DB_PASSWORD", 'PHN7kTLQdseAAe');
+    define("DB_HOST", 'sql309.infinityfree.com');
+    define("DB_PORT", '3306');
+    define("SYS_URL", 'https://testgoldenbac.lovestoblog.com');
+    define("DEBUGGING", false);
+    define("DEFAULT_LOCALE", 'en_us');
+    define("LICENCE_KEY", 'xxxxxxxx');
+    define("JWT_SECRET", '0b0cfc07fca81c956ab9181d8576f4a8');
+    ?>
